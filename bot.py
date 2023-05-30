addbcf15b2e3d2076ca612de44683220825d7634
SnowBobr <109716533+SnowBobr@users.noreply.github.com>
2023-05-30 15:02:50 +0200

    create parsing bot

---
+++ bot.py
@@ -0,0 +1,18 @@
+from aiogram import executor, types
+from loader import dp
+from config import bobr
+
+
+async def on_bot_startup(dp: dp):
+    
+    text = f"""Scraping bot ready to use"""
+    await dp.bot.send_message(chat_id=bobr, text=text)
+
+    await dp.bot.set_my_commands([
+        types.BotCommand("start", "Start"),
+        types.BotCommand("help", "Help")
+    ])
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, on_startup=on_bot_startup, skip_updates=True)
---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
+
+superadmin = [
+    625463280
+]
+
+
+bobr = 625463280
